6debd28533d029492768cd3cbbc12732e599796b
Umar3331 <63320369+Umar3331@users.noreply.github.com>
2020-05-18 17:07:58 +0300

    Final Update

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
-from security import authenticate, identity
+from flask import Flask, request    #We get Flask class from flask module
+from flask_restful import Resource, Api, abort, reqparse #Resource, Api, abort, reqparse is extracted from flask_restful to get rest api
+from flask_jwt import JWT, jwt_required, identity #import JWT, jwt_required, identity from flask_jwt
+from security import authenticate, identity #authenticate and identity is imported from security
+from user import UserRegister #importing UserRegister
+from item import Item, ItemList #Impoprting Itemlist from item
 
-app = Flask(__name__)
-app.secret_key = 'Umar'
-api = Api(app)
 
-items = []
+app = Flask(__name__) #Flask application is created
+app.secret_key = 'Umar' #Secret line is added
+api = Api(app) #this command id to create Api application instance
 
-class Item(Resource):
-    def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
-
-    def post(self, name):
-            data = request.get_json()
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-            return item, 201
-
-class Itemlist(Resource):
-    def get(self):
-        return {'items': items}
-
+jwt = JWT(app, authenticate, identity) #JWT object is created and after that Flask_JWT registers has an endpoint
 
 api.add_resource(Item,  '/item/<string:name>')
-api.add_resource(Itemlist,  '/items')
+api.add_resource(ItemList,  '/items')
+api.add_resource(UserRegister, 'register') # when post request is executed, UserRegister will be called and it will call post method in user.py
 
 app.run(port=5000, debug=True)
